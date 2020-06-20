7d4b21f012233012e0696cfefc8b760ec0eec539
yc <2247684782@qq.com>
2020-06-20 16:44:42 +0800

    6.20

---
+++ src/main/java/yc/collection/TestMap01.java
@@ -0,0 +1,9 @@
+package yc.collection;
+
+import java.util.HashMap;
+
+public class TestMap01 {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+    }
+}
--- src/main/java/yc/jianZhiOffer/JZ_17.java
+++ src/main/java/yc/jianZhiOffer/JZ_17.java
@@ -3,7 +3,7 @@ package yc.jianZhiOffer;
 public class JZ_17 {
     //输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
 
-    public boolean HasSubtree(TreeNode A, TreeNode B) {
+    /*public boolean HasSubtree(TreeNode A, TreeNode B) {
         //特例 A树和B树为空时 直接返回false
         if(A == null || B == null)
             return false;
@@ -28,5 +28,31 @@ public class JZ_17 {
             return false;
         }
 
+    }*/
+
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        if(root1 == null || root2 == null){
+            return false;
+        }
+        if(root1.val == root2.val && (buid(root1.left,root2.left) && buid(root1.right,root2.right))){
+            return true;
+        }
+        return HasSubtree(root1.left,root2) || HasSubtree(root1.right,root2);
+
     }
+
+    public boolean buid (TreeNode root1, TreeNode root2){
+        if(root2 == null)
+            return true;
+        if(root1 == null)
+            return false;
+        if(root1.val == root2.val){
+            return buid(root1.left,root2.left)&&buid(root1.right,root2.right);
+        }else{
+            return false;
+        }
+    }
+
+
+
 }
